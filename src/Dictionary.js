4ec77d615dbc858a8cbe990837b745a7fbc4eed5
mbutto26 <102936996+mbutto26@users.noreply.github.com>
2022-10-18 09:45:36 -0700

    Axios & dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
+  function handleResponse(response) {}
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
